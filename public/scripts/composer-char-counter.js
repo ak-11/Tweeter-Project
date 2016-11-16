3b35240a25b5ccbc09a806256aca4e0bd9db2871
Andrew Kim <your@email.com>
2016-11-16 15:32:20 -0500

    Refactored code with Harry Mills. Learned about chaining the code and using . at the end to have them continue on multiple lines.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,10 @@
 
-$(document).ready(function () {
-    $("#compose-tweet").on("keyup", function(event) {
-      let newTweetCounter = 140 - $(this).val().length;
-      $(this).siblings("span.counter").text(newTweetCounter);
-        if (newTweetCounter < 0) {
-          $("span.counter").addClass("warning-character-max");
-        } else {
-          $("span.counter").removeClass("warning-character-max");
-        }
-    })
+$(document).ready(function() {
+  $("#compose-tweet").on("keyup", function(event) {
+    let newTweetCounter = 140 - $(this).val().length;
+    $(this).siblings(".counter").
+      text(newTweetCounter).
+      toggleClass("warning-character-max", newTweetCounter < 0);
+  })
 });
-// Remember to use Jquery calls
 
